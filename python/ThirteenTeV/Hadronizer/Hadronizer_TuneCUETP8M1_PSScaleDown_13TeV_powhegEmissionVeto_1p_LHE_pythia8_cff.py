2cecc0c8b4ea05c50434885713c475d76a917ef2
qianying guo <qyguo@buaa.edu.cn>
2020-01-04 00:42:01 +0100

    TuneCUETP8M1 tune and scale varibles

---
+++ python/ThirteenTeV/Hadronizer/Hadronizer_TuneCUETP8M1_PSScaleDown_13TeV_powhegEmissionVeto_1p_LHE_pythia8_cff.py
@@ -0,0 +1,38 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+from Configuration.Generator.Pythia8PowhegEmissionVetoSettings_cfi import *
+
+pythia8ScaleVarSettingsBlock = cms.PSet(
+    pythia8ScaleVarSettings = cms.vstring(
+       'TimeShower:renormMultFac = 0.25',
+       'SpaceShower:renormMultFac = 0.25'
+    )
+)
+
+generator = cms.EDFilter("Pythia8HadronizerFilter",
+                         maxEventsToPrint = cms.untracked.int32(1),
+                         pythiaPylistVerbosity = cms.untracked.int32(1),
+                         filterEfficiency = cms.untracked.double(1.0),
+                         pythiaHepMCVerbosity = cms.untracked.bool(False),
+                         comEnergy = cms.double(13000.),
+                         PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        pythia8PowhegEmissionVetoSettingsBlock,
+        pythia8ScaleVarSettingsBlock,
+        processParameters = cms.vstring(
+            'POWHEG:nFinal = 1', ## Number of final state particles
+                                 ## (BEFORE THE DECAYS) in the LHE
+                                 ## other than emitted extra parton
+          ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CUEP8M1Settings',
+                                    'pythia8PowhegEmissionVetoSettings',
+                                    'pythia8ScaleVarSettings',
+                                    'processParameters'
+                                    )
+        )
+                         )
+
+ProductionFilterSequence = cms.Sequence(generator)
